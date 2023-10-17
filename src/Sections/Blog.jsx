de1ad17a0cf460a364b16eadcb938980ac42b087
fettahaud <fettahaud@gmail.com>
2023-10-17 11:35:27 +0300

    Done Blog

---
+++ src/Sections/Blog.jsx
@@ -0,0 +1,11 @@
+export default function Blog() {
+  return (
+    <div id="blog">
+      <h1>blog</h1>
+      <figure>
+        <img src="/blog-frame.png" alt="Blog" />
+        <img src="/noise.png" alt="noise" />
+      </figure>
+    </div>
+  );
+}
